4ca5ee57a28a36030be73a0f42e9a196022c01c1
Rafa Couto <rafacsilva016@gmail.com>
2024-08-30 11:06:12 -0300

    Removed unused import

--- lib/features/widget_test/presentation/pages/widget_test_page.dart
+++ lib/features/widget_test/presentation/pages/widget_test_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex/core/components/text_header/text_header.dart';
-import 'package:pokedex/core/components/text_header/text_header_component.dart';
 
 class WidgetTestPage extends StatefulWidget {
   const WidgetTestPage({super.key});
